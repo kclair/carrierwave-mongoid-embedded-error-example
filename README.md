000ff055dd2bb92ac7ffdc7e69fab6baa11410ac
K. Clair <kclair@g2llc.com>
2012-05-11 13:52:13 -0700

    initialize rails application; with mongoid

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 carrierwave-mongoid-embedded-error-example
 ==========================================
+
+This is an attempt to produce a failing test case to prove https://github.com/jnicklas/carrierwave-mongoid/issues/42
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveMongoidEmbeddedErrorExample::Application.config.secret_token = '2e5a3f3f1feb9a4c47f6cf86b347241fcae1bb415fb86c2484cfcd9b6c15d47e87400139b110c74d6624f1533c65f86a16d690764cf607c7d6b9a532f2d998b6'
